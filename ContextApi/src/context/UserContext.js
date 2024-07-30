e1ed9074422c8390b946844c3204aa69f7479de0
AbdulRauf010 <abdulraufchoudhry17@gmail.com>
2024-07-30 15:56:16 +0500

    ContextApi

---
+++ ContextApi/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// Create Context and after creating it we need to make a provider for that as well
+
+const UserContext = React.createContext();
+
+export default UserContext;
